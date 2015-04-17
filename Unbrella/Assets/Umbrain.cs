7ba8a0ee9748d898c3838a0e2482ac0a55e1ffa5
Darth-Carrotpie <carrotpie@gmail.com>
2015-04-17 21:30:55 +0300

    SPawning many cubes

---
+++ Unbrella/Assets/Umbrain.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Umbrain : MonoBehaviour {
+
+	public GameObject umbrella;
+	float timer = 5;
+	public float rainSpawnPeriod;
+	public float destroyTimer;
+	public int mapMatrixY;
+	public int mapMatrixX;
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		timer+=Time.deltaTime;
+		if(timer>rainSpawnPeriod){
+			timer=0;
+			 
+			GameObject newRainPoint = Instantiate (umbrella, GetRainSpawnPoint(), Quaternion.identity) as GameObject;
+			Destroy(newRainPoint, destroyTimer);
+		}
+	}
+
+	public Vector3 GetRainSpawnPoint(){
+		Vector3 spawnPoint = new Vector3();
+		float x = Random.Range(0, mapMatrixX) - mapMatrixX/2;
+		float y = Random.Range(0, mapMatrixY) - mapMatrixY/2;
+		Debug.Log("x: "+x + "      y: "+y);
+		spawnPoint = new Vector3(x, 0, y);
+		return spawnPoint;
+	}
+}
