7ea14f447787576efdd61b1ff02199736114859c
Darth-Carrotpie <carrotpie@gmail.com>
2015-04-17 21:46:44 +0300

    Some rain system added

--- Unbrella/Assets/Umbrain.cs
+++ Unbrella/Assets/Umbrain.cs
@@ -31,6 +31,7 @@ public class Umbrain : MonoBehaviour {
 		float y = Random.Range(0, mapMatrixY) - mapMatrixY/2;
 		Debug.Log("x: "+x + "      y: "+y);
 		spawnPoint = new Vector3(x, 0, y);
+		spawnPoint = spawnPoint*5;
 		return spawnPoint;
 	}
 }
