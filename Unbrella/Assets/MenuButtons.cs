b0bd1595db77acbb913a690e96206345c9ecab4f
Kristonitas <Kristupas.stumbrys@gmail.com>
2015-04-18 09:44:43 +0300

    menu things

---
+++ Unbrella/Assets/MenuButtons.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void toPlay(){
+		Application.LoadLevel ("MainScene");
+	}
+}
