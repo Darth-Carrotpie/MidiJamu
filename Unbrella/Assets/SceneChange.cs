7b032bdac9e86929a0c622131d9c982eaf73007b
Darth-Carrotpie <carrotpie@gmail.com>
2015-04-18 15:14:51 +0300

    ANim fix of text

--- Unbrella/Assets/SceneChange.cs
+++ Unbrella/Assets/SceneChange.cs
@@ -14,7 +14,7 @@ public class SceneChange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Score.ScoreGathered > 1000){
+		if(Score.ScoreGathered > 100){
 			sceneInstanceCount++;
 
 			animBcgkImg = GameObject.Find("Canvas").transform.FindChild("Animation Bckg").gameObject;
