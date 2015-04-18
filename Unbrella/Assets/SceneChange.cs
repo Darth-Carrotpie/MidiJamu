09c03499c4d7e927ee0acea14b24e6c9cfa4c357
Darth-Carrotpie <carrotpie@gmail.com>
2015-04-18 13:16:25 +0300

    ANimation activation from script when score is enough

--- Unbrella/Assets/SceneChange.cs
+++ Unbrella/Assets/SceneChange.cs
@@ -2,14 +2,30 @@
 using System.Collections;
 
 public class SceneChange : MonoBehaviour {
-
+	public static int sceneInstanceCount;
+	GameObject animBcgkImg;
+	float waitCounter;
 	// Use this for initialization
 	void Start () {
+		sceneInstanceCount = 0;
 		DontDestroyOnLoad(this);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Score.ScoreGathered > 1000){
+			sceneInstanceCount++;
+
+			animBcgkImg = GameObject.Find("Canvas").transform.FindChild("Animation Bckg").gameObject;
+			animBcgkImg.SetActive(true);
+			waitCounter+=Time.deltaTime;
+		}
+
+		if(waitCounter > 7.6f){
+			Score.ScoreGathered = 0;
+			waitCounter=0f;
+			Application.LoadLevel("MainScene");
+		}
 	}
 }
