bd08990ad0d08c6b812d9e3e0840887169c4b794
Darth-Carrotpie <carrotpie@gmail.com>
2015-04-18 05:30:20 +0300

    Added Rainbow Spawn generator

---
+++ Unbrella/Assets/ConsumeARainbow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsumeARainbow : MonoBehaviour {
+	RainbowManager rainbowMng;
+	void Start () {
+		GameObject dumpFolder = GameObject.Find("Buildings");
+		dumpFolder = dumpFolder.transform.FindChild("Drains").gameObject;
+		rainbowMng =  dumpFolder.GetComponent<RainbowManager>() as RainbowManager;
+	}
+	
+	void Update () {
+	
+	}
+	void OnTriggerEnter( Collider col){
+		rainbowMng.aDumpIsActive = false;
+		GameObject colHit = col.gameObject;
+		Destroy(colHit);
+
+	}
+
+}
